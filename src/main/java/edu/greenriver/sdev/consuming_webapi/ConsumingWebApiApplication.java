56b9cb3d9a1e8613142f7ca83706dd39c000d4ea
fauziaameeri <ameeri.fauzia@student.greenriver.edu>
2023-02-13 17:57:45 -0800

    Consuming Web Api

---
+++ src/main/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.consuming_webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.consuming_webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
