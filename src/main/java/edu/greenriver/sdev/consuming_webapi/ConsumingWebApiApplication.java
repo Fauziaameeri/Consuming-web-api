e7bf866ee973150231fcc47eb4cc56a1c5121623
fauziaameeri <ameeri.fauzia@student.greenriver.edu>
2023-02-13 19:46:25 -0800

    Added an index with style sheet

--- src/main/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplication.java
+++ src/main/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsumingWebApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumingWebApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingWebApiApplication.class, args);
+    }
 
 }
--- src/test/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/consuming_webapi/ConsumingWebApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConsumingWebApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
